9ea23267edefeb313c8e391e9bcf7868c96fad64
J4nis05 <janis.roelli@gmail.com>
2025-01-04 22:27:14 +0100

    Code Refactor, Image Resize Fixes

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-from PIL import Image, ImageTk
-import ffmpeg
+#region Imports
+# ==============================================================================
 import cv2
+import ffmpeg
+from PIL import Image, ImageTk
 
+import mimetypes
 import os
 import random
-import tkinter
-import mimetypes
+import tkinter as tk
 import uuid
-
-#region Base Values
-input_dir     = "videos"
-output_dir    = "wallpapers"
-export_prefix = "wallpaper"
-window_width  = 1920
-window_height = 1080
+# ==============================================================================
 
 
-#region Select Video from List
-def select_video(video_list: list, frame_dict: dict):
-    video = random.choice(video_list)
-    frame = random.randint(0, frame_dict[video])
-    return video, frame
+#region Functions
+# ==============================================================================
 
-
-#region Is File a Video
-def is_video(path: str) -> bool:
-    mime_type, _ = mimetypes.guess_type(path)
-    return mime_type is not None and mime_type.startswith("video/")
-
-
-#region Get Files from in dir
-def get_files(path: str):
+def get_files(path: str) -> list:
     files = []
     all_items = os.listdir(path)
 
@@ -42,8 +27,19 @@ def get_files(path: str):
     return files
 
 
-#region Get Frames from Video
-def get_frame_count(file_path: str):
+def is_video(path: str) -> bool:
+    mime_type, _ = mimetypes.guess_type(path)
+    return mime_type is not None and mime_type.startswith("video/")
+
+
+def select_video(frame_dict: dict) -> tuple[str, int]:
+    keys = list(frame_dict.keys())
+    video = random.choice(keys)
+    frame = random.randint(0, frame_dict[video])
+    return video, frame
+
+
+def get_frame_count(file_path: str) -> int:
     metadata = ffmpeg.probe(file_path)
     streams = [ s for s in metadata['streams'] if s['codec_type'] == 'video' ]
     video_stream = streams[0]
@@ -51,8 +47,7 @@ def get_frame_count(file_path: str):
     return frame_count
 
 
-#region Extract Frame
-def get_frame(file: str, frame: int):
+def extract_frame(file: str, frame: int) -> Image.Image | None:
     try:
         capture = cv2.VideoCapture(file)
         capture.set(cv2.CAP_PROP_POS_FRAMES, frame)
@@ -66,81 +61,48 @@ def get_frame(file: str, frame: int):
         capture.release()
 
 
-#region Get Videos and Frames
-# ==============================================================================
+def resize_image(image, win_width = 1920, win_height = 1080) -> Image.Image:
+    image_width, image_height = image.size
 
-dir_path      = f"{ input_dir }/"
-scanned_files = get_files(dir_path)
-files         = [ file for file in scanned_files if is_video(file) ]
-frame_dict    = { file: int(get_frame_count(file)) for file in files }
+    width_scale = win_width / image_width
+    height_scale = win_height / image_height
+    scale_factor = min(width_scale, height_scale)
+    
+    new_width = int(image_width * scale_factor)
+    new_height = int(image_height * scale_factor)
+    return image.resize((new_width, new_height), Image.Resampling.LANCZOS)
 
-# ==============================================================================
 
-
-#region Get Wallpaper
-# (Select a File & Frame and get the Extracted Frame)
-def get_wallpaper():
-    selected_video, selected_frame = select_video(files, frame_dict)
-    extracted_frame = get_frame(selected_video, selected_frame)
+def get_wallpaper() -> Image.Image | None:
+    selected_video, selected_frame = select_video(frame_dict)
+    extracted_frame = extract_frame(selected_video, selected_frame)
     return extracted_frame
 
 
-#region Resize to Fit in GUI
-def resize_image(image, window_width = 1920):
-    image_width, image_height = image.size
-
-    if image_width > window_width:
-        new_height = int((window_width / image_width) * image_height)
-        return image.resize((window_width, new_height), Image.Resampling.LANCZOS)
-    return image
-
-
-#region Tkinter Window
-# ==============================================================================
-
-window = tkinter.Tk()
-window.title = "Wallpapers"
-window.geometry(f"{window_width}x{window_height}")
-
-current_frame    = get_wallpaper()
-resized_frame    = resize_image(current_frame, window_width)
-previous_frame   = None
-next_frame       = None
-is_previous      = False
-button_width     = 10
-frame_index      = 0
-wallpaper_frames = []
-wallpaper_frames.append(current_frame)
-
-
-#region Previous Image
-def button1_click():
-    global current_frame, previous_frame, next_frame, wallpaper_frames, frame_index
+def prev_image():
+    global current_frame, wallpaper_frames, frame_index
 
     if frame_index == 0:
-        wallpaper_frames.insert(0, get_wallpaper())
+        return
     else:
         frame_index -= 1
 
     current_frame = wallpaper_frames[frame_index]
-
-    resized = resize_image(current_frame, window.winfo_width())
+    resized = resize_image(current_frame, window.winfo_width(), window.winfo_height())
     tk_image = ImageTk.PhotoImage(resized)
     image_label.config(image=tk_image)
     image_label.image = tk_image
 
 
-#region Save Image
-def button2_click():
+def save_image():
     global current_frame
     filename = f"{output_dir}/{export_prefix}_{uuid.uuid4()}.png"
     current_frame.save(filename)
     print(f"Saved Image to {filename}")
 
 
-#region Next Image
-def button3_click():
-    global current_frame, next_frame, wallpaper_frames, frame_index
+def next_image():
+    global current_frame, wallpaper_frames, frame_index
 
     if frame_index+1 == len(wallpaper_frames):
         wallpaper_frames.append(get_wallpaper())
@@ -148,52 +110,91 @@ def button3_click():
     frame_index += 1
     current_frame = wallpaper_frames[frame_index]
 
-    resized = resize_image(current_frame, window.winfo_width())
+    resized = resize_image(current_frame, window.winfo_width(), window.winfo_height())
     tk_image = ImageTk.PhotoImage(resized)
     image_label.config(image=tk_image)
     image_label.image = tk_image
 
 
-#region Key Handlers
 def key_handler_previous_image(event):
-    button1_click()
+    prev_image()
 
 
 def key_handler_save_image(event):
-    button2_click()
+    save_image()
 
 
 def key_handler_next_image(event):
-    button3_click()
+    next_image()
 
 
-#region Window Layout
-button_frame = tkinter.Frame(window)
-button_frame.pack(side=tkinter.TOP, fill=tkinter.X)
-image_frame = tkinter.Frame(window)
-image_frame.pack(expand=True, fill=tkinter.BOTH)
-buttons_container = tkinter.Frame(button_frame)
-buttons_container.pack(expand=True)
+def key_handler_quit(event):
+    window.quit()
 
-#region Buttons
-button1 = tkinter.Button(buttons_container, command=button1_click, width=button_width, text="Previous")
-button2 = tkinter.Button(buttons_container, command=button2_click, width=button_width, text="Save")
-button3 = tkinter.Button(buttons_container, command=button3_click, width=button_width, text="Next")
-button1.pack(side=tkinter.LEFT, padx=20, pady=10)
-button2.pack(side=tkinter.LEFT, padx=20, pady=10)
-button3.pack(side=tkinter.LEFT, padx=20, pady=10)
+# ==============================================================================
 
-#region Load Image into GUI
+
+#region Base Values
+# ==============================================================================
+
+input_dir        = "videos"
+output_dir       = "wallpapers"
+export_prefix    = "wallpaper"
+window_width     = 1920
+window_height    = 1080
+btn_width        = 10
+frame_index      = 0
+dir_path         = os.path.join(input_dir)
+scanned_files    = get_files(dir_path)
+files            = [ file for file in scanned_files if is_video(file) ]
+frame_dict       = { file: int(get_frame_count(file)) for file in files }
+current_frame    = get_wallpaper()
+resized_frame    = resize_image(current_frame, window_width, window_height)
+wallpaper_frames = []
+wallpaper_frames.append(current_frame)
+
+# ==============================================================================
+
+
+#region Tkinter Window
+# ==============================================================================
+
+window = tk.Tk()
+window.title = "Wallpapers"
+window.geometry(f"{window_width}x{window_height}")
+
+
+button_frame = tk.Frame(window)
+button_frame.pack(side=tk.TOP, fill=tk.X)
+
+image_frame = tk.Frame(window)
+image_frame.pack(expand=True, fill=tk.BOTH)
+
+btn_container = tk.Frame(button_frame)
+btn_container.pack(expand=True)
+
+
+button1 = tk.Button(btn_container, command=prev_image, width=btn_width, text="Previous")
+button2 = tk.Button(btn_container, command=save_image, width=btn_width, text="Save")
+button3 = tk.Button(btn_container, command=next_image, width=btn_width, text="Next")
+
+button1.pack(side=tk.LEFT, padx=20, pady=10)
+button2.pack(side=tk.LEFT, padx=20, pady=10)
+button3.pack(side=tk.LEFT, padx=20, pady=10)
+
+
 tk_image = ImageTk.PhotoImage(resized_frame)
-image_label = tkinter.Label(image_frame)
+image_label = tk.Label(image_frame)
 image_label.pack(expand=True)
 image_label.config(image=tk_image)
 image_label.image = tk_image
 
+
 window.bind("<Left>",   key_handler_previous_image)
 window.bind("<Return>", key_handler_save_image)
 window.bind("<Right>",  key_handler_next_image)
-window.bind("<Escape>", lambda e: window.quit())
+window.bind("<Escape>", key_handler_quit)
+
 
 window.mainloop()
 
